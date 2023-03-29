804564ca4e369574ea5d39ed90bb64cbb0b3e4f3
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-03-29 19:01:29 +0300

    Setup retrofit

---
+++ app/src/main/java/com/vlyashuk/newsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.vlyashuk.newsapp
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/vlyashuk/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.vlyashuk.newsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "da059436f54340439d6b5bf3fa297519"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
