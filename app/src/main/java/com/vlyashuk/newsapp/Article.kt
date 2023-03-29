7e32a398347f9d0e053712b18aea4fd49a9e788e
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-03-29 19:42:38 +0300

    DAO and Article setup

--- app/src/main/java/com/vlyashuk/newsapp/Article.kt
+++ app/src/main/java/com/vlyashuk/newsapp/Article.kt
@@ -1,6 +1,15 @@
 package com.vlyashuk.newsapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
