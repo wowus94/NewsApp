9b0c128b886ed1c9d8e8ec19ee16b8fc72a698f4
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-10-09 22:36:57 +0300

    start

--- app/src/main/java/com/vlyashuk/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/vlyashuk/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -56,6 +56,9 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
             )
         }
 
+
+
+
         viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
             when (response) {
                 is Resource.Success -> {
